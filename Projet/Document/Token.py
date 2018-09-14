e98089bca39ed119d07d2a430b980fc2e51dbb69
Pattachou <chou.95230@live.fr>
2018-09-14 16:52:35 +0200

    modifiy token +add sentences

--- Projet/Document/Token.py
+++ Projet/Document/Token.py
@@ -52,17 +52,3 @@ def __repr__(self):
     return 'Token({}, {}, {})'.format(self.text, self.start, self.end)
 
 
-class Sentence(Interval):
-    """ Interval corresponding to a Sentence"""
-
-    def __init__(self, document, start: int, end: int):
-        Interval.__init__(self, start, end)
-        self._doc = document
-
-    def __repr__(self):
-        return 'Sentence({}, {})'.format(self.start, self.end)
-
-    @property
-    def tokens(self):
-        """Returns the list of tokens contained in a sentence"""
-        # TODO: To be implemented (tip: use Interval.overlap)
